f66acf2b7cb5e81dd96fd22044964d8842b7cd92
Sherard Bailey <sbailey@xogrp.com>
2016-03-06 22:26:42 -0500

    Add form view specs

--- specs/javascripts/views/assistance_form_spec.js
+++ specs/javascripts/views/assistance_form_spec.js
@@ -1,25 +1,98 @@
 describe("Assistance Form", function(){
   var assistanceForm, serviceTypes;
-  var serviceTypeID = "employment";
-  var serviceTypeDisplayName = "Employment";
 
-  beforeEach(function(){
-    var models = [{
-      id: serviceTypeID, 
-      display_name: serviceTypeDisplayName
-    }];
-    serviceTypes = new window.App.Collections.ServiceTypes(models);
+  describe('Populate service type dropdown', function(){
+    var serviceTypeID = "employment";
+    var serviceTypeDisplayName = "Employment";
+
+    beforeEach(function(){
+      var models = [{
+        id: serviceTypeID, 
+        display_name: serviceTypeDisplayName
+      }];
+      serviceTypes = new window.App.Collections.ServiceTypes(models);
+    });
+
+    it('populates the service-type select dropdown with options', function(){
+      setFixtures('<form class="assistance"><select class="service-types"></select></form>');
+      assistanceForm = new window.App.Views.AssistanceForm({
+        el: "form.assistance",
+        collection: serviceTypes
+      });
+      assistanceForm.populateServiceTypes();
+
+      expect($('select.service-types option')).toHaveAttr('value', serviceTypeID);
+      expect($('select.service-types option')).toHaveText(serviceTypeDisplayName);
+    });
   });
 
-  it('populates the service-type select dropdown with options', function(){
-    setFixtures('<form class="assistance"><select class="service-types"></select></form>');
-    assistanceForm = new window.App.Views.AssistanceForm({
-      el: "form.assistance",
-      collection: serviceTypes
-    });
-    assistanceForm.populateServiceTypes();
+  describe('Form Management', function(){
+    var formMarkup;
 
-    expect($('select.service-types option')).toHaveAttr('value', serviceTypeID);
-    expect($('select.service-types option')).toHaveText(serviceTypeDisplayName);
+    describe('Enabled and disabled states', function(){
+      it('disables the form', function(){
+        formMarkup = '<form class="assistance">'
+            + '<div class="submit">'
+              + '<input type="submit">'
+            + '</div>'
+          + '</form>';
+
+        setFixtures(formMarkup);
+        assistanceForm = new window.App.Views.AssistanceForm({
+          el: "form.assistance"
+        });
+        assistanceForm.disableForm();
+
+        expect(assistanceForm.submitBtn).toHaveAttr('disabled');
+      });
+
+      it('enables the form', function(){
+        formMarkup = '<form class="assistance">'
+            + '<div class="submit">'
+              + '<input type="submit" disabled="disabled">'
+            + '</div>'
+          + '</form>';
+
+        setFixtures(formMarkup);
+        assistanceForm = new window.App.Views.AssistanceForm({
+          el: "form.assistance"
+        });
+        assistanceForm.enableForm();
+
+        expect(assistanceForm.submitBtn).not.toHaveAttr('disabled');
+      });
+    });
+
+    describe('Clean up', function(){
+      it('cleans up error states', function(){
+        formMarkup = '<form class="assistance">'
+            + '<div class="form-group has-error"></div>'
+            + '<div class="form-group"></div>'
+            + '<div class="form-group has-error"></div>'
+          + '</form>';
+
+        setFixtures(formMarkup);
+        assistanceForm = new window.App.Views.AssistanceForm({
+          el: "form.assistance"
+        });
+        assistanceForm.cleanupForm();
+
+        expect($('.has-error')).not.toBeInDOM();
+      });
+
+      it('cleans up alert states', function(){
+        formMarkup = '<form class="assistance">'
+            + '<div class="alert alert-danger"></div>'
+          + '</form>';
+
+        setFixtures(formMarkup);
+        assistanceForm = new window.App.Views.AssistanceForm({
+          el: "form.assistance"
+        });
+        assistanceForm.cleanupForm();
+
+        expect(assistanceForm.alert).not.toHaveClass('alert-danger');
+      });
+    });
   });
 });
